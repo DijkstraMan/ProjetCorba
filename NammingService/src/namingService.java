d2ab2fdee8bcedf0ced5530c021637b5cf1728f4
NicoBicha <boyer_n@outlook.com>
2016-06-24 00:41:41 +0200

    LE VRAI DERNIER COMMIT (j'espere) DONNEZ MOI VOS ENERGIES OUAAAAAAAAAAAAAAAAAAAAAA

--- NammingService/src/namingService.java
+++ NammingService/src/namingService.java
@@ -9,8 +9,8 @@ public class namingService {
 
     public static void main(String args[]) {
        String []t= new String[2];
-         //t[0]="-ORBPort=20017";
-         t[0]="-ORBPort=2001";
+         t[0]="-ORBPort=20017";
+        // t[0]="-ORBPort=2001"; // pour le local
          t[1]="-print";
          MapNamingContext.main(t);
     }
