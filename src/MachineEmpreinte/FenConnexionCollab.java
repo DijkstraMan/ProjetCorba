9564a3d421d45483110bec10525e0c36282be6b8
Ramuncho <dejean.rom@gmail.com>
2016-06-23 09:10:54 +0200

    Maj jeu de données (ajout type time pour horaire)

--- src/MachineEmpreinte/FenConnexionCollab.java
+++ src/MachineEmpreinte/FenConnexionCollab.java
@@ -9,7 +9,6 @@ import Accueil.FenConnexionAccueil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import modEntreesSortiesZones.EmpreinteInconnue;
 import modEntreesSortiesZones.ServiceAuthentification;
 import modEntreesSortiesZones.ServiceEmpreinte;
 import modEntreesSortiesZones.UtilisateurInconnu;
@@ -263,15 +262,11 @@ public class FenConnexionCollab extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FenConnexionCollab.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FenConnexionCollab.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FenConnexionCollab.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(FenConnexionCollab.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the form */
